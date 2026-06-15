f42f4248d82dc3b79a8aa4d7795fb501569f572f
smuffels <bruncel1@students.zhaw.ch>
2026-06-15 18:38:19 +0200

    add basics for iphone view

--- health_but_better/health_but_better/View/HearRateView.swift
+++ health_but_better/health_but_better/View/HearRateView.swift
@@ -6,12 +6,50 @@ import SwiftUI
 
 struct HeartRateView: View {
     @StateObject var vm = HeartRateViewModel()
+    @State private var selectedDate = Date()
+    @State private var showDatePicker = false
+    
+    
 
+   
+    
     var body: some View {
-        List(vm.samples) { sample in
-            Text("\(sample.bpm) bpm – \(sample.date_time.formatted())- \(sample.category)")
+        VStack{
+            //Date and Date picker
+            Text(selectedDate.formatted(date: .long, time: .omitted))
+                .padding()
+                .onTapGesture {
+                    showDatePicker = true
+                }
+                .popover(isPresented: $showDatePicker) {
+                    DatePicker(
+                        "Select Date",
+                        selection: $selectedDate,
+                        displayedComponents: [.date]
+                    )
+                    .datePickerStyle(.graphical)
+                    .onChange(of: selectedDate){
+                        showDatePicker = false
+                    }
+                }
+            // List
+            List(vm.samples){sample in
+                HStack{
+                    Text(sample.date_time.formatted(
+                        date: .numeric,// Todo: change to omitted
+                        time: .shortened
+                    ))
+                    Spacer()
+                    Text("\(sample.bpm) bpm")
+                    
+                    // just for checking if filters are correct
+                    Spacer()
+                    Text("\(sample.category)")
+                }
+                
+            }.task { await vm.load()
+            }
         }
-        .task { await vm.load() }
     }
 }
 
