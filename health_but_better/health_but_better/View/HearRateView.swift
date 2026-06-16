a39c687b36c85d662c96547b8d8a4de7c8bdb9bc
smuffels <bruncel1@students.zhaw.ch>
2026-06-16 20:58:55 +0200

    update view

--- health_but_better/health_but_better/View/HearRateView.swift
+++ health_but_better/health_but_better/View/HearRateView.swift
@@ -6,7 +6,6 @@ import SwiftUI
 
 struct HeartRateView: View {
     @StateObject var vm = HeartRateViewModel()
-    @State private var selectedDate = Date()
     @State private var showDatePicker = false
     
     
@@ -16,7 +15,7 @@ struct HeartRateView: View {
     var body: some View {
         VStack{
             //Date and Date picker
-            Text(selectedDate.formatted(date: .long, time: .omitted))
+            Text(vm.selectedDate.formatted(date: .long, time: .omitted))
                 .padding()
                 .onTapGesture {
                     showDatePicker = true
@@ -24,32 +23,95 @@ struct HeartRateView: View {
                 .popover(isPresented: $showDatePicker) {
                     DatePicker(
                         "Select Date",
-                        selection: $selectedDate,
+                        selection: $vm.selectedDate,
                         displayedComponents: [.date]
                     )
                     .datePickerStyle(.graphical)
-                    .onChange(of: selectedDate){
+                    .onChange(of: vm.selectedDate){
                         showDatePicker = false
                     }
                 }
-            // List
-            List(vm.samples){sample in
-                HStack{
-                    Text(sample.date_time.formatted(
-                        date: .numeric,// Todo: change to omitted
-                        time: .shortened
-                    ))
-                    Spacer()
-                    Text("\(sample.bpm) bpm")
-                    
-                    // just for checking if filters are correct
-                    Spacer()
-                    Text("\(sample.category)")
+            
+            HStack(alignment: .top){
+                // Links
+                VStack(alignment: .leading){
+                    // Picker left
+                    Picker("Category", selection: $vm.selectedCategory){
+                        Text("Ruhezustand").tag(HeartRateCategory.sedentary)
+                        Text("Aktiv").tag(HeartRateCategory.active)
+                        Text("Weiss nonig").tag(HeartRateCategory.notSet)
+                        Text("Keine Kategorie").tag(HeartRateCategory.noData)
+                    }
+                    // List left
+                    List(vm.filteredSamples){sample in
+                        HStack{
+                            Text(sample.date_time.formatted(
+                                date: .omitted,
+                                time: .shortened
+                            ))
+                            Spacer()
+                            Text("\(sample.bpm) bpm")
+                            
+                            // just for checking if filters are correct
+                            //Spacer()
+                            //Text("\(sample.category)")
+                        }
+            
+                    }.task { await vm.load()
                 }
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            // Rechts
+                VStack(alignment: .leading){
+                    
+                        // Picker right
+                        Picker("Category", selection: $vm.selectedCategoryBPM){
+                            Text("Ruhezustand").tag(HeartRateCategory.sedentary)
+                            Text("Aktiv").tag(HeartRateCategory.active)
+                            Text("Weiss nonig").tag(HeartRateCategory.notSet)
+                            Text("Keine Kategorie").tag(HeartRateCategory.noData)
+                        }
+                    HStack{
+                        Picker("operator", selection: $vm.selectedOperator){
+                            Text(">=").tag(ComparisonOperator.greaterEqual)
+                            Text("<=").tag(ComparisonOperator.lessEqual)
+
+                        }
+                        Picker("BPM", selection: $vm.selectedBPM){
+                            Text("110").tag(110)
+                            Text("100").tag(100)
+                            Text("90").tag(90)
+                            Text("80").tag(80)
+                        }
+                    }
+                        
+                    
+                   
+                    
+                    
+                    // List right
+                    List(vm.filteredSamplesBPM){sample in
+                        HStack{
+                            Text(sample.date_time.formatted(
+                                date: .omitted,
+                                time: .shortened
+                            ))
+                            Spacer()
+                            Text("\(sample.bpm) bpm")
+                            
+                            // just for checking if filters are correct
+                            Spacer()
+                            Text("\(sample.category)")
+                        }
+                        
+                    }.task { await vm.load()}
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 
-            }.task { await vm.load()
+                
+                
+                
             }
-        }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
